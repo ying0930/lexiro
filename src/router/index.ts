047f29e5e6b3085a0ff1de5735ca1ad7c2571ca7
ying0930 <tavricccc@gmail.com>
2026-05-27 23:54:10 +0800

    feat: disable router auto-scroll to prevent page transition jitter

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(),
+  scrollBehavior: () => false,
   routes: [
     {
       path: '/',
