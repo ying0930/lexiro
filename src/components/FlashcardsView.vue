6d91caada2061b77902cdee438d0b5ca3003aa35
Ying Tsai <tavricccc@gmail.com>
2026-06-04 00:32:53 +0000

    Ga

--- src/components/FlashcardsView.vue
+++ src/components/FlashcardsView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
-import { useVirtualList } from '@/lib/useVirtualList'
+import { computed } from 'vue'
 import { useSessionStore } from '@/stores/session'
 import { useSetsStore } from '@/stores/sets'
 import FlashcardView from './FlashcardView.vue'
@@ -9,20 +8,16 @@ import FlashcardView from './FlashcardView.vue'
 const { activeSet } = storeToRefs(useSetsStore())
 const { currentSession, sessionEntries } = storeToRefs(useSessionStore())
 
-const topSentinel = ref<HTMLElement | null>(null)
-const bottomSentinel = ref<HTMLElement | null>(null)
-const { visibleItems, windowStart } = useVirtualList(sessionEntries, topSentinel, bottomSentinel)
+const orderedEntries = computed(() => [...sessionEntries.value].sort((left, right) => left.originalIndex - right.originalIndex))
 </script>
 
 <template>
   <section v-if="activeSet && currentSession" class="space-y-6">
-    <div ref="topSentinel" class="h-1" />
     <FlashcardView
-      v-for="(entry, i) in visibleItems"
-      :key="entry.item.id"
+      v-for="entry in orderedEntries"
+      :key="entry.originalIndex"
       :item="entry.item"
-      :index="windowStart + i"
+      :index="entry.originalIndex"
     />
-    <div ref="bottomSentinel" class="h-1 -translate-y-4 shadow-none opacity-0" />
   </section>
 </template>
